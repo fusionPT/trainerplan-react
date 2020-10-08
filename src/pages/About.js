bd372dd7791c36275b5936c2e20689a54860ebde
Bruno Felicio <gigafusion@gmail.com>
2020-10-08 16:18:37 +0200

    Adds page contet

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,10 @@ import { Layout } from '../layout/Layout';
 
 const About = () => (
     <Layout>
-        This is the About page!
+        <div class="w-1/2">
+            <h1 class="text-gray-900 text-3xl font-semibold leading-10 pb-5">About</h1>
+            <p class="text-gray-700 leading-6"></p>
+        </div>
     </Layout>
 )
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,10 @@ import { Layout } from '../layout/Layout';
 
 const Contact = () => (
     <Layout>
-        This is the Contact page!
+        <div class="w-1/2">
+            <h1 class="text-gray-900 text-3xl font-semibold leading-10 pb-5">Contact Us</h1>
+            <p class="text-gray-700 leading-6"></p>
+        </div>
     </Layout>
 )
 
